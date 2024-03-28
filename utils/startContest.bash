2f9fa813eeac8bce01aa759fcb262bfc2e874253
Saul Canche <saulcanche.sc8@gmail.com>
2024-03-27 22:02:00 -0600

    se añadio una nueva carpeta y un scrip para crear los archivos de un concurso nuevo

---
+++ utils/startContest.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+contest="$1"
+cd ../contests || exit 1
+mkdir "$contest" || exit 1
+cd "$contest" || exit 1
+
+for problem in $(eval echo {$2..$3}); do
+    if [ -f "$problem.cpp" ]; then
+        continue
+    else
+        touch "$problem".cpp
+        # Insert template content into the newly created file
+        cat <<EOF > "$problem".cpp
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve(){
+}
+int main(){
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    solve();
+}
+EOF
+    fi
+done
+code .
