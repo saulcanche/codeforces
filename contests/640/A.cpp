9224d48b4e9ca1042514233f20a830ed2dd8d698
Saul Canche <saulcanche.sc8@gmail.com>
2024-04-05 00:38:35 -0600

    se añadio un nuevo concurso

---
+++ contests/640/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve(){
+    ll n; cin >> n;
+    string strN = to_string(n);
+    for (ll i = 0; i < strN.length; i++)
+    {    
+        n/
+            
+    }
+    
+
+
+}
+int main(){
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    ll t; cin >> t;
+    global vector<ll> multiples10(1e5)
+    for (ll i = 0; i < 1e5; i++)
+    {
+        multiples10[i] * 10;
+    }
+    
+    while (t--)
+    {
+        solve();
+    }
+    
+}
