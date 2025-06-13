a5738e4c0c8df38be7b8a8d6bd9f1fa01e6a5c26
Saul Canche <saulcanche.sc8@gmail.com>
2025-06-13 15:48:26 -0600

    se añadio un nuevo script para hacer un virtual ICPC like contest

---
+++ utils/startVirtual.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+contest="$1"
+cd ../contests || exit 1
+mkdir "$contest" || exit 1
+cd "$contest" || exit 1
+
+for problem in $(eval echo {$2..$3}); do
+    if [ -f "$problem.cpp" ]; then
+        continue
+    else
+        touch "$problem".cpp
+        cat <<EOF > "$problem".cpp
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+EOF
+    fi
+done
+code .
